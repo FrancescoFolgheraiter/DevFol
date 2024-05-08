e7da9dab3594498bd4467968a258825f0abbb65b
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2024-05-08 18:49:49 +0200

    add router link to the project

---
+++ router.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router';
+// import IndexPage from './src/components/pages/indexPage.vue';
+// import RestaurantPage from './src/components/pages/RestaurantPage.vue';
+// import PaymentPage from './src/components/pages/PaymentPage.vue';
+// import LavoraConNoiPage from './src/components/pages/LavoraConNoi.vue';
+// import OrderSuccess from './src/components/pages/OrderSuccess.vue';
+// import NotFoundPage from './src/components/pages/NotFound.vue'; 
+
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: IndexPage 
+    // }
+  ]
+});
+
+export {router};
