23d06f0e8ed4fa880b9bcff6e813c9ff2de181e1
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2024-05-09 10:42:12 +0200

    add technologies

--- src/components/Pages/HomePage.vue
+++ src/components/Pages/HomePage.vue
@@ -3,7 +3,18 @@
     export default{
         data(){
             return{
-
+                //my knowledge
+                technologies:[
+                    "HTML 5",
+                    "CSS/SCSS",
+                    "Bootstrap 5",
+                    "Javascript",
+                    "Vue",
+                    "PHP",
+                    "Laravel",
+                    "MySQL",
+                    "Git"
+                ]
             };
         },
         methods:{
@@ -22,7 +33,7 @@
         <div class="bg-gray-200 p-4 text-center">
             <p>Ciao! Piacere di conoscerti 👋</p>
             <h1 class="text-base sm:text-xl font-semibold sm:font-normal">
-                Sono Francesco, un full-stack web developer.
+                Sono Francesco, un full-stack web developer di Trento(IT).
             </h1>
         </div>
         <div class="bg-gray-200 p-4 text-center">
@@ -34,7 +45,7 @@
             Immagine
         </div>
         <div class="bg-gray-200 p-4 text-center">
-            Breve bio 
+            Sono un giovane developer 
         </div>
     </section>
     <section class="grid grid-cols-2 gap-4 mb-4">
@@ -42,7 +53,11 @@
             knowledge
         </div>
         <div class="bg-gray-200 p-4 text-center">
-            lista delle tecnologie
+            <ul class="grid grid-cols-3 gap-2">
+                <li v-for="technology in technologies">
+                    {{ technology }}
+                </li>
+            </ul>
         </div>
     </section>
 </template>
