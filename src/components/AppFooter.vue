6ef3e4209fcf9349fdfa38d1b625383b16d696b0
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2024-05-13 13:50:54 +0200

    add logo to footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -5,6 +5,9 @@
         };
         },
         methods:{
+            getImagePath(imgPath){
+			    return new URL (imgPath, import.meta.url).href;
+		    }
         }
     }
 </script>
@@ -12,7 +15,11 @@
 <template>
     <div class=" min-h-[300px] bg-slate-900 text-white">
         <div class="grid grid-cols-1 sm:grid-cols-4 gap-12 text-center container">
-            <div>Logo Francesco Folgheraiter</div>
+            <div class="flex-grow h-[100px]">
+            <router-link :to="{name:'home'}">
+                <img class=" h-full" :src="getImagePath('/img/LogoFrancescoFolgheraiter.png')" alt="">
+            </router-link>
+            </div>
             <div>Pages</div>
             <div>Contacts</div>
             <div>footer</div>
